5bdc914830ca71ad312e89a4ba10cca8c53b9593
jcescobarn <jcescobarn@unal.edu.co>
2025-10-23 22:14:11 +0000

    fix errors

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -15,16 +15,3 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
     key = "error.html"
   }
 }
-
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
-    }]
-  })
-}
