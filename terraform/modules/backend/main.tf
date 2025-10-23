1f004313fad57239cfc58761fec33a3ab5b8edd9
jcescobarn <jcescobarn@unal.edu.co>
2025-10-23 21:54:23 +0000

    fix

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -57,8 +57,8 @@ resource "aws_api_gateway_integration" "upload_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.upload.id
   http_method             = aws_api_gateway_method.upload_post.http_method
-  integration_http_method = "AWS_PROXY"
-  type                    = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.upload_url_lambda.invoke_arn
 }
 
